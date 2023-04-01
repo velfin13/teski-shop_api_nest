095287dcebf0378b689e0429bd0819b9d371aff6
Velfin Velasquez <velfinvelasquez@gmail.com>
2023-04-01 18:00:21 -0500

    variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,24 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'velkin',
-    password: 'password',
-    database: 'teski-shop',
-    autoLoadEntities: true,
-    synchronize: true,
-  }), ProductsModule,],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+    }), ProductsModule,],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    console.log(process.env.DB_PASSWORD);
+
+  }
+}
