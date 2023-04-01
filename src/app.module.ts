1ae93ee9748c044bca22c095ece22c7f7f70e9c4
Velfin Velasquez <velfinvelasquez@gmail.com>
2023-03-31 23:16:12 -0500

    creacion de recurso de productos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'velkin',
+    password: 'password',
+    database: 'teski-shop',
+    autoLoadEntities: true,
+    synchronize: true,
+  }), ProductsModule,],
   controllers: [],
   providers: [],
 })
-export class AppModule { }
+export class AppModule {}
